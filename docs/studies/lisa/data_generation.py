fixed variance of noise, signal generation, looks fine.

docs/studies/lisa/data_generation.py
@@ -53,7 +53,7 @@ INCLUDE_GALACTIC = os.getenv("LISA_INCLUDE_GALACTIC", "1").strip().lower() not i
     "no",
     "off",
 }
-RNG_SEED = int(os.getenv("LISA_SEED", "2"))
+RNG_SEED = int(os.getenv("LISA_SEED", "0"))
 
 
 def _save_plot_once_to_cache(stem: str, draw_plot) -> None:
@@ -303,8 +303,8 @@ def draw_rfft_from_psd(
     white = rng.normal(size=psd.shape) + 1j * rng.normal(size=psd.shape)
     coeffs = np.sqrt(np.maximum(psd, 0.0) / (4.0 * df * dt**2)) * white
     if coeffs.size:
-        coeffs[0] = coeffs[0].real
-        coeffs[-1] = coeffs[-1].real
+        coeffs[0] = np.sqrt(2) * coeffs[0].real
+        coeffs[-1] = np.sqrt(2) * coeffs[-1].real
     return coeffs
 
 
@@ -542,9 +542,9 @@ def main():
         gEf = np.zeros(n_freqs, dtype=complex)
         gTf = np.zeros(n_freqs, dtype=complex)
 
-    a_bg_f = nAf + gAf
-    e_bg_f = nEf + gEf
-    t_bg_f = nTf + gTf
+    a_bg_f = 0*(nAf + gAf)
+    e_bg_f = 0*(nEf + gEf)
+    t_bg_f = 0*(nTf + gTf)
 
     plt.loglog(freqs_all, np.abs(nAf) ** 2, label="A Noise", alpha=0.5)
     if INCLUDE_GALACTIC:

docs/studies/lisa/gb_prior.py
@@ -10,20 +10,23 @@ import numpy as np
 
 SOURCE_CATALOG = np.array(
     [
-        [1.35962e-3, 8.94581279e-19, 1.07345e-22, 2.40, 0.31, 3.56, 0.52, 3.06],
-        [1.41220e-3, 2.30000000e-18, 8.20000000e-23, 2.15, 0.18, 1.20, 0.93, 1.40],
+        # [1.35962e-3, 8.94581279e-19, 1.07345e-22, 2.40, 0.31, 3.56, 0.52, 3.06],
+        # [1.41220e-3, 2.30000000e-18, 8.20000000e-23, 2.15, 0.18, 1.20, 0.93, 1.40],
+        [1.35962e-3, 8.94581279e-15, 1.07345e-20, 2.40, 0.31, 3.56, 0.52, 3.06],
+        [1.41220e-3, 2.30000000e-12, 8.20000000e-21, 2.15, 0.18, 1.20, 0.93, 1.40],
     ],
     dtype=float,
 )
 
 F0_GLOBAL_BOUNDS = (
-    float(SOURCE_CATALOG[:, 0].min() - 1.5e-5),
-    float(SOURCE_CATALOG[:, 0].max() + 1.5e-5),
+    float(SOURCE_CATALOG[:, 0].min() - 1.5e-7),
+    float(SOURCE_CATALOG[:, 0].max() + 1.5e-7),
 )
 # FDOT_GLOBAL_BOUNDS = (5.0e-19, 4.0e-18)
-FDOT_GLOBAL_BOUNDS = (5.0e-19, 8.0e-18)
+# FDOT_GLOBAL_BOUNDS = (5.0e-19, 8.0e-18)
+FDOT_GLOBAL_BOUNDS = (5.0e-17, 8.0e-13)
 FIXED_FDOT_PRIOR_BOUNDS = FDOT_GLOBAL_BOUNDS
-FIXED_A_PRIOR_BOUNDS = (6.0e-24, 1.7e-23)
+FIXED_A_PRIOR_BOUNDS = (6.0e-25, 1.7e-22)
 F0_REF = float(np.mean(SOURCE_CATALOG[:, 0]))
 
 